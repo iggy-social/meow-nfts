b473e7092c2bd293d6b68e44b328450df8104b06
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2024-10-09 18:56:44 +0200

    FAQ enabled

--- components/sidebars/SidebarLeft.vue
+++ components/sidebars/SidebarLeft.vue
@@ -213,13 +213,12 @@
             </NuxtLink>
           </li>
 
-          <!-- FAQ 
+          <!-- FAQ -->
           <li class="nav-item p-1" @click="closeLeftSidebar">
-            <a class="nav-link" href="https://docs.google.com/document/d/1UTTFLszVAwpd7LbvNMbj4vL1NBaN0ImoZYNPJ4T_EJo/edit?usp=sharing" target="_blank">
+            <a class="nav-link" href="https://docs.google.com/document/d/1P4xopjkRMDBqr8R1Gk_szD51y_oa0dJeCBKLHyUyqZc/edit" target="_blank">
               <i class="bi bi-patch-question me-1"></i> FAQ <small><i class="bi bi-box-arrow-up-right ms-1"></i></small>
             </a>
           </li>
-          -->
 
           <!-- Dapps List 
           <li class="nav-item p-1" @click="closeLeftSidebar">
